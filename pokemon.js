95f539478e885965a2d724d004e467a64ce72663
astrokwk <awykmok@gmail.com>
2018-12-11 00:15:32 -0500

    made code neater

--- pokemon.js
+++ pokemon.js
@@ -1,343 +1,200 @@
 $(document).ready(function() {
-
-var url = "https://pokeapi.co/api/v2/pokemon/";
-
-var pokemon = document.getElementById('pokemon');
-var button = document.getElementById('button');
-var inputVal = document.getElementById('input').value;
-var input = inputVal.toLowerCase();
-
-var pokeURL = url + input;
-
+	var button = document.getElementById('button');
 	
 //--Animation of input box when clicked
 	$('.searchBox').click(function(){
-	
 		$('#input').animate({width: 'show', right: '100'});
-
 	});
 	
 
-button.addEventListener('click', function pokeSubmit(){
+	button.addEventListener('click', function pokeSubmit(){
 		var paramVal = document.getElementById("input").value;
     var param = paramVal.toLowerCase();
-    /*var pokeURL = "https://pokeapi.co/api/v2/pokemon-form/" + param*/;
-	var uRLl = "https://pokeapi.co/api/v2/pokemon-form/";
-	var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + param;
-		var entries = "https://pokeapi.co/api/v2/pokemon-species/" + param;
-		var pokemon = document.getElementById('pokemon');
-	var po = document.getElementById('po');
-	var mo = document.getElementById('mo');
+		var uRLl = "https://pokeapi.co/api/v2/pokemon-form/";
+		var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + param;
+		var po = document.getElementById('po');
+		var mo = document.getElementById('mo');
 	
 	
+		//Status
+		var success = false;
 	
-				var weight = document.createElement('div');
-				var height = document.createElement('div');
-				var type = document.createElement('div');
-				var habitat = document.createElement('div');
-	
-	
-	
-	//Status
-	var success = false;
-	
-	
-	
-	const t0 = performance.now();
-
-
+		const t0 = performance.now();
 
 	
-	    $.getJSON(pokeURL, function(data){
-				
+	  $.getJSON(pokeURL, function(data){
 			
 			//Status succeed
 			success = true;
 			
-        console.log(data);
-        console.log(JSON.stringify(data, null, "  "));
+      // console.log(data);
+      // console.log(JSON.stringify(data, null, "  "));
 				
-				$(`#numName`).html(`#` + data.id + `. `+ data.name);
-			
-
+			$(`#numName`).html(`#` + data.id + `. `+ data.name);
 				
-				for (var mey in data.forms) {
-					 var formURL = data.forms[mey].url;
+			for (var mey in data.forms) {
+				
+				var formURL = data.forms[mey].url;
+				var img = document.createElement('img');
+				img.setAttribute('src', data.sprites.front_default);
 					
-													var img = document.createElement('img');
-										img.setAttribute('src', data.sprites.front_default);
-					
-									$.getJSON(formURL, function(e){
-
-
-										
-										if (data.sprites.front_default == null) {
-											img.setAttribute('src', e.sprites.front_default);
-										}
-										
-
-			po.appendChild(img);
-					
-				})
+				$.getJSON(formURL, function(e){
+															
+					if (data.sprites.front_default == null) {
+						img.setAttribute('src', e.sprites.front_default);
+						}
+					po.appendChild(img);
+					});
 				}
-				
-				
-				
-				var entry = data.species.url;
-				
-				
-				
-				
-				    $.getJSON(entry, function(r){
+										
+			var entry = data.species.url;
+									
+			$.getJSON(entry, function(r){
+				success = true;
 			
-			success = true;
-			
-        console.log(r);
-        console.log(JSON.stringify(r, null, "  "));
-			
-
-			
-			
-			var pokemonEntries = document.createElement('p');
-			pokemonEntries.setAttribute('id', 'description' );
-				
-				
-				
+        // console.log(r);
+        // console.log(JSON.stringify(r, null, "  "));
+				var pokemonEntries = document.createElement('p');
+				pokemonEntries.setAttribute('id', 'description' );
+						
 				//looping through an array inside a Json object
 				for (var key in r.flavor_text_entries) {
 					if (r.flavor_text_entries[key].language.name == "en" && r.flavor_text_entries[key].version.name == "moon") {
-
 						mo.innerHTML = r.flavor_text_entries[key].flavor_text;
 						break;
 					} else if (r.flavor_text_entries[key].language.name == "en" && r.flavor_text_entries[key].version.name == "x") {
 						mo.innerHTML = r.flavor_text_entries[key].flavor_text;
 					}
-					
 				}
-							
-				
-				//Abilites			
-							
-							
-
-							
-							
-				
-
-				
-			
-
-
-			mo.appendChild(pokemonEntries);
-							
-				
-			var evol1 = document.createElement('div');
-							var evol1P = document.createElement('img');
-							
-			var evol2 = document.createElement('img');
-							var evol2P = document.createElement('div');
-							
-							var evol3P = document.createElement('div');
-			var evol3 = document.createElement('img');
-				
-							$(`#eva`).empty();
-							
-					$.getJSON(r.evolution_chain.url, function(d){
-						console.log(d);
-						console.log(JSON.stringify(d, null, "  "));
-						
-						console.log(d.chain.evolves_to);
-						console.log(d.chain.species.name);
-						
-						
-						
-										$.getJSON(uRLl + d.chain.species.name, function(m){
-											console.log(m);
-											
-											
-											evol1P.setAttribute('class', "evol1");
-											evol1P.setAttribute('src', m.sprites.front_default);
-												evol1.setAttribute('style', 'order: 1;');
-				
-											
-											$(`#eva`).append(evol1);
-											$(evol1).append(evol1P);
-
-										})
-							
-						
-						
-						
-						
-							var chiii = d.chain.evolves_to;
-						
-							if (chiii.length < 1) {
-								console.log("No evolution at all");
-							} else {
 								
-								for (var meat in d.chain.evolves_to){
-									var meek = d.chain.evolves_to[meat];
+				//Abilites			
+				
+				mo.appendChild(pokemonEntries);
+							
+				var evol1 = document.createElement('div');
+				var evol1P = document.createElement('img');
+							
+				var evol2 = document.createElement('img');
+				var evol2P = document.createElement('div');
+							
+				var evol3P = document.createElement('div');
+				var evol3 = document.createElement('img');
+				
+				$(`#eva`).empty();
+							
+				$.getJSON(r.evolution_chain.url, function(d){
+					// console.log(d);
+					// console.log(JSON.stringify(d, null, "  "));
+					// console.log(d.chain.evolves_to);
+					// console.log(d.chain.species.name);
+					$.getJSON(uRLl + d.chain.species.name, function(m){
+						// console.log(m);		
+						evol1P.setAttribute('class', "evol1");
+						evol1P.setAttribute('src', m.sprites.front_default);
+						evol1.setAttribute('style', 'order: 1;');
 									
-										if (meek.length < 1) {
-										console.log("No more evolution");
+						$(`#eva`).append(evol1);
+						$(evol1).append(evol1P);
+						});
+							
+				var chiii = d.chain.evolves_to;
+						
+				if (chiii.length < 1) {
+					console.log("No evolution at all");
+					} else {		
+						for (var meat in d.chain.evolves_to){
+							var meek = d.chain.evolves_to[meat];
+		
+							if (meek.length < 1) {
+								console.log("No more evolution");
 									} else {
-										console.log('More evolution');
-										console.log(d.chain.evolves_to[meat].species.name);
-										
-										
-										
-										
-										$.getJSON(uRLl + d.chain.evolves_to[meat].species.name, function(n){
-											console.log(n);
-											
-											
-											evol2.setAttribute('class', 'evol2');
-											evol2.setAttribute('src', n.sprites.front_default);
+										// console.log('More evolution');
+										// console.log(d.chain.evolves_to[meat].species.name);	
+									$.getJSON(uRLl + d.chain.evolves_to[meat].species.name, function(n){
+										// console.log(n);	
+										evol2.setAttribute('class', 'evol2');
+										evol2.setAttribute('src', n.sprites.front_default);
 
-												evol2P.setAttribute('style', 'order: 2;');
+										evol2P.setAttribute('style', 'order: 2;');
 
-											$(`#eva`).append(evol2P);
-											$(evol2P).append(evol2);
-											
-										})
+										$(`#eva`).append(evol2P);
+										$(evol2P).append(evol2);
+										});
 										
+									for(var peter in d.chain.evolves_to[meat].evolves_to) {
 										
-										
-
-										
-										
-											for(var peter in d.chain.evolves_to[meat].evolves_to) {
-
-											var chuuu = d.chain.evolves_to[meat].evolves_to[peter];
+										var chuuu = d.chain.evolves_to[meat].evolves_to[peter];
 												
-											if (chuuu.length < 1) {
-												console.log('no evolution');
+										if (chuuu.length < 1) {
+											console.log('no evolution');
 											} else {
-											console.log(d.chain.evolves_to[meat].evolves_to[peter].species.name);
-												
-												
-												
-												
-											$.getJSON(uRLl + d.chain.evolves_to[meat].evolves_to[peter].species.name, function(o){
-											console.log(o);
-											
-
-										 evol3.setAttribute('class', 'evol3');
-
-											evol3.setAttribute('src', o.sprites.front_default);
-												
-												
-
+												// console.log(d.chain.evolves_to[meat].evolves_to[peter].species.name);
+												$.getJSON(uRLl + d.chain.evolves_to[meat].evolves_to[peter].species.name, function(o){
+												// console.log(o);
+										 		evol3.setAttribute('class', 'evol3');
+												evol3.setAttribute('src', o.sprites.front_default);
 												evol3P.setAttribute('style', 'order: 3;');
 												
-											$(`#eva`).append(evol3P);
+												$(`#eva`).append(evol3P);
 												$(evol3P).append(evol3);
-											
-										})
-											}
-							}
-		
-
-	
+										});
 										}
 									}
-
 								}
-							});
-							
-							
-							
-							var bebe = [];
-							for (var grum in data.abilities) {
-								/*if(data.abilities[grum] < data.abilities.length) results.push(data.abilities[grum].ability.name);*/
-								bebe.push(data.abilities[grum].ability.name);
 							}
+						}
+					});
 							
-							console.log(bebe);
+										
+				var bebe = [];
+				for (var grum in data.abilities) {
+					/*if(data.abilities[grum] < data.abilities.length) results.push(data.abilities[grum].ability.name);*/
+					bebe.push(data.abilities[grum].ability.name);
+						}
+					// console.log(bebe);		
+					document.getElementById('abili').innerHTML = bebe.join(", ");
 							
-							document.getElementById('abili').innerHTML = bebe.join(", ");
-							
-							
-
 			});
 				
 				
-				
-
-
-				
 //Calculating Height				
 				
-						console.log(data.height);		
-		var heightNumber = data.height/10;		
-				
-			console.log(heightNumber * 3.28 * 12);	
-				
-				
+		// console.log(data.height);		
+		var heightNumber = data.height/10;			
+		// console.log(heightNumber * 3.28 * 12);			
 		var inches = heightNumber * 3.28 * 12;
     var feet = Math.floor(inches / 12);
     inches %= 12;
 		var inchesRound = Math.round(inches);
 		var feetRound = feet + 1;
-				
-
-				
-				
-				
-			
-				
+								
 		if (inchesRound === 12){
-
 			$('#heightt').html(feetRound + `'` +  `0"`);
-		}		else {
-
-
+		}	else {
 			$('#heightt').html(feet + `'` + inchesRound + `"`);
-			
 		}
 				
+		
+	//Calculating Weight		
+		var weightNumber = data.weight/10;	
+		var pounds = weightNumber * 2.205;
+		var realpounds = Number(pounds.toFixed(1)).toFixed(1);			
 				
-				
-	//Calculating Weight	
-				
-				
-	var weightNumber = data.weight/10;
-				
-	var pounds = weightNumber * 2.205;
-	var realpounds = Number(pounds.toFixed(1)).toFixed(1);			
-				
-				
-
-	$(`#weightt`).html(realpounds + `lbs`);
-				
-
-	
-				
-function weightPkmn() {
-	
-}
-				
+		$(`#weightt`).html(realpounds + `lbs`);
 
 //Type of Pokemon
 				
 		var info4 = document.createElement('p');	
 		var typee = document.getElementById(`typee`);
-				
-				
+						
 		for(var pey in data.types) {
 			if (data.types.length == 2 ) {
-
 				$(`#typee`).html(data.types[1].type.name + ` / ` + data.types[0].type.name);
-
 			} else {
-
 				$(`#typee`).html(data.types[0].type.name);
 			}
 			
 			if(data.types[pey].slot == 1) {
-				
-			
-
 				switch (data.types[pey].type.name) {
 					case "fire":
 						$('nav').css('background', 'linear-gradient(to bottom, #fe8c00, #f83600)');
@@ -429,133 +286,87 @@ function weightPkmn() {
 						$(`.bio`).css(`color`, `#757f9a`);
 						$('#evolution').css('background-color', `#757f9a`);
 						break;
-						
-					
-				}
-						
-				console.log (data.types[pey].type.name);
+				}	
+				// console.log (data.types[pey].type.name);
 			}
-			
-			
 		}		
-				
-				
-				
-				
-	
-				
-				
-				
+
 				
 //Habitat				
 				
-		 $.getJSON(entry, function(resp){		
-		 				
-							
-			var info5 = document.createElement('p');
-			 var habitatt = document.getElementById('habitatt');
-			 
+		$.getJSON(entry, function(resp){		
 			
+			var info5 = document.createElement('p');
+			var habitatt = document.getElementById('habitatt');
 
-			 if(resp.habitat != null) {
-
-				 $(`#habitatt`).html(resp.habitat.name);
-				 console.log(resp.habitat.name);	
-			 } else {
-
-				  $(`#habitatt`).html('unknown');
-			 }
-
-		 
+			if (resp.habitat != null) {
+				$(`#habitatt`).html(resp.habitat.name);
+				console.log(resp.habitat.name);
+				} else {
+					$(`#habitatt`).html('unknown');
+				}
 		 });
-				
-
-
     });
 	
 	
-	
-
-	
-	console.log('chicken');
+	// console.log('chicken');
 	
 	// Set a 5-second (or however long you want) timeout to check for errors
-setTimeout(function() {
-    if (!success)
-    {
-        // Handle error accordingly
-        alert("Yup, 404"); // string was "Houston, we have a problem."
-    }
-}, 10000);
+	setTimeout(function() {
+    if (!success){
+      // Handle error accordingly
+      alert("Yup, 404"); // string was "Houston, we have a problem."
+		}
+	}, 30000);
 	
 	$(`#input`).val('');
 	
-
-	
-				while(po.firstChild){
+	while(po.firstChild){
     po.removeChild(po.firstChild);
 }
 	
-
-	
-
-	
 	//prevent page refresh
-
-
-	
-	const t1 = performance.now();
-	console.log("This code took " + (t1 - t0) + " milliseconds.");
-	 });
-
-
-	
-	
+	// const t1 = performance.now();
+	// console.log("This code took " + (t1 - t0) + " milliseconds.");
+	});
 	
 	
 	button.addEventListener('click', function() {
+
+		var ability = document.createElement('div');
+		ability.setAttribute('class', 'abb');
+		$(ability).insertAfter('#pokemon');
+		$(ability).html(`<span class="bio" style="margin-left: 0px;">Abilites: </span> <p id="abili"></p>`);
 		
-						var ability = document.createElement('div');
-						ability.setAttribute('class', 'abb');
-						$(ability).insertAfter('#pokemon');
-				
-				$(ability).html(`<span class="bio" style="margin-left: 0px;">Abilites: </span> <p id="abili"></p>`);
-		
-$('<hr>').insertAfter(ability);
-		
-		
+		$('<hr>').insertAfter(ability);
 						
-					var meme = document.createElement('div');
-				meme.setAttribute('class', 'info-container');
-				$('.screen').append(meme);
+		var meme = document.createElement('div');
+		meme.setAttribute('class', 'info-container');
+		$('.screen').append(meme);
 				
-				var weight = document.createElement('div');
-				var height = document.createElement('div');
-				var type = document.createElement('div');
-				var habitat = document.createElement('div');
-	
-			
-				var infoList = [height, type, weight, habitat];
+		var weight = document.createElement('div');
+		var height = document.createElement('div');
+		var type = document.createElement('div');
+		var habitat = document.createElement('div');
+
+		var infoList = [height, type, weight, habitat];
 				
-				for (var i = 0; i < infoList.length; i++) {
-					infoList[i].setAttribute('class', 'info-box');
-					infoList[i].setAttribute('id', i);
-					
-					
-					$(meme).append(infoList[i]);
-				}
+		for (var i = 0; i < infoList.length; i++) {
+
+			infoList[i].setAttribute('class', 'info-box');
+			infoList[i].setAttribute('id', i);
+						
+			$(meme).append(infoList[i]);
+			}
 				
 		$(weight).html('<span class="bio">Weight: </span> <p id="weightt"></p>');
 		$(height).html(`<span class="bio">Height: </span> <p id="heightt"></p>`);
 		$(type).html(`<span class="bio">Type: </span> <p id="typee"></p>`);
 		$(habitat).html(`<span class="bio">Habitat: </span> <p id="habitatt"></p>`);
 		
-		
-		
 		var evolution = document.createElement('div');
 		$(evolution).html('Evolution');
 		evolution.setAttribute('id', 'evolution');
-		
 
 		$(evolution).insertAfter(`.info-container`);
 		
@@ -563,26 +374,17 @@ $('<hr>').insertAfter(ability);
 		evoo.setAttribute('id', 'eva');
 		$(evoo).insertAfter(evolution);
 		
-
 	}, {once : true});
 	
 
-
-
-			
-			//Enter key when pressed activate the click event
-		$(`#input`).keyup(function (e) {
-			
-
+	//Enter key when pressed activate the click event
+	$(`#input`).keyup(function (e) {
 		if (e.keyCode === 13) {
 			$(`#button`).click();
-			console.log('monkey');
-					e.preventDefault();
-					}
-		});
-
+			e.preventDefault();
+		}
+	});
 	
-	
-	} );
+});
 
 
